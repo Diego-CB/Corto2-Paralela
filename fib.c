964485cc8da5f600fe75d5b805b69e522e70db21
Diego Córdova <60157586+Diego-CB@users.noreply.github.com>
2023-08-30 21:16:19 -0600

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,67 +1,76 @@
-#include <stdlib.h>
+#include <omp.h>
 #include <stdio.h>
-#include <time.h>       // for clock_t, clock(), CLOCKS_PER_SEC
-#include <unistd.h>     // for sleep()
+#include <stdlib.h>
+#include <time.h>   // for clock_t, clock(), CLOCKS_PER_SEC
+#include <unistd.h> // for sleep()
 
 int fib_recursive(int n);
 int fib_recursive_omp(int n);
+int fib_recursive_omp_fix(int n);
 
-int main(int argc, char* argv) {
-    int n = 0;
+int main(int argc, char **argv) {
+  int n = 0;
 
-    if (argc > 1) {
-        n = atoi(argv[1]);
-    }
+  if (argc > 1) {
+    n = atoi(argv[1]);
+  }
 
-    double time_spent = 0.0;
-    clock_t begin = clock();
-    int fib_calculado = fib_recursive(n);
-    clock_t end = clock();
+  double time_spent = 0.0;
+  clock_t begin = clock();
+  int fib_calculado = fib_recursive_omp_fix(n);
+  clock_t end = clock();
 
-    printf("Fib para %d es %d", n, fib_calculado);
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Tiempo de ejecucion %f seconds", time_spent);
-}   
+  printf("Fib para %d es %d\n", n, fib_calculado);
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Tiempo de ejecucion %f seconds\n", time_spent);
+}
 
 int fib_recursive_omp_fix(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1);
-        return 1
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+
+    omp_set_num_threads(4);
     
-    int fib_part1
+    int fib_part1;
     #pragma omp task shared(fib_part1) private(n) final(n)
     fib_part1 = fib_recursive(n - 1);
-
-    int fib_part2
+  
+    int fib_part2;
     #pragma omp task shared(fib_part2) private(n) final(n)
     fib_part2 = fib_recursive(n - 2);
-
-    #pragma omp taskwait
-    return fib_part1 + fib_part2
+  
+    return fib_part1 + fib_part2;
+  }
 }
 
 int fib_recursive_omp(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1);
-        return 1
-
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    int fib_part1;
     #pragma omp task
-    int fib_part1 = fib_recursive(n - 1);
+    fib_part1 =  fib_recursive(n - 1);
     
+    int fib_part2;
     #pragma omp task
-    int fib_part2 = fib_recursive(n - 2);
-
+    fib_part2 = fib_recursive(n - 2);
+  
     #pragma omp taskwait
-    return fib_part1 + fib_part2
+    return fib_part1 + fib_part2;
+  }
 }
 
 int fib_recursive(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1);
-        return 1
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  }
 
-    return fib_recursive(n - 1) + fib_recursive(n - 2);
+  return fib_recursive(n - 1) + fib_recursive(n - 2);
 }
